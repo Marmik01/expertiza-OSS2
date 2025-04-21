c54fae7ec16432a0cb92aaaa1abc5ecc07c438c2
vdeo <vdeo@ncsu.edu>
2025-04-21 19:36:11 -0400

    added edge case in survey deploy spec

--- spec/models/survey_deployment_spec.rb
+++ spec/models/survey_deployment_spec.rb
@@ -40,6 +40,13 @@ RSpec.describe SurveyDeployment, type: :model do
       survey_deployment = SurveyDeployment.new(start_date: Time.now, end_date: Time.now + 1.day, questionnaire_id: 98, parent_id: 12112, type: 'CourseSurveyDeployment')
       expect(survey_deployment).to be_valid
     end
+
+    it 'is valid when start_date and end_date are the same' do
+      now = Time.now
+      survey_deployment = SurveyDeployment.new(start_date: now, end_date: now, questionnaire_id: 98, parent_id: 12112, type: 'CourseSurveyDeployment')
+      expect(survey_deployment).to be_valid
+    end
+
   end
 
   describe 'abstract methods' do
