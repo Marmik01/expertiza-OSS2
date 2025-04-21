c37267e777164e3eb9a831d3b717189dfa336db7
vdeo <vdeo@ncsu.edu>
2025-04-21 19:16:01 -0400

    Refined few test cases in survey deployment spec for better understanding. Added additional test case for response_map method

--- spec/models/survey_deployment_spec.rb
+++ spec/models/survey_deployment_spec.rb
@@ -7,50 +7,56 @@ RSpec.describe SurveyDeployment, type: :model do
   let(:response_map) { build(:review_response_map, reviewer_id: 2) }
   describe 'validations' do
     it 'is invalid without a start_date' do
-      sd = SurveyDeployment.new(end_date: Time.now + 1.day, questionnaire_id: 985, parent_id: '12345678', type: 'CourseSurveyDeployment')
+      sd = SurveyDeployment.new(end_date: Time.now + 1.day, questionnaire_id: 98, parent_id: 12112, type: 'CourseSurveyDeployment')
       expect(sd).not_to be_valid
       expect(sd.errors[:start_date]).to include("can't be blank")
     end
 
     it 'is invalid without an end_date' do
-      sd = SurveyDeployment.new(start_date: Time.now)
+      sd = SurveyDeployment.new(start_date: Time.now, questionnaire_id: 98, parent_id: 12112, type: 'CourseSurveyDeployment')
       expect(sd).not_to be_valid
       expect(sd.errors[:end_date]).to include("can't be blank")
     end
 
     it 'is invalid when both start_date and end_date are nil' do
-      sd = SurveyDeployment.new
+      sd = SurveyDeployment.new(questionnaire_id: 98, parent_id: 12112, type: 'CourseSurveyDeployment')
       expect(sd).not_to be_valid
       expect(sd.errors[:base]).to include('The start and end time should be specified.')
     end
 
     it 'is invalid if end_date is before start_date' do
-      sd = SurveyDeployment.new(start_date: Time.now + 2.days, end_date: Time.now + 1.day)
+      sd = SurveyDeployment.new(start_date: Time.now + 2.days, end_date: Time.now + 1.day, questionnaire_id: 98, parent_id: 12112, type: 'CourseSurveyDeployment')
       expect(sd).not_to be_valid
       expect(sd.errors[:base]).to include('The End Date should be after the Start Date.')
     end
 
     it 'is invalid if end_date is in the past' do
-      sd = SurveyDeployment.new(start_date: Time.now - 1.day, end_date: Time.now - 1.hour)
+      sd = SurveyDeployment.new(start_date: Time.now - 1.day, end_date: Time.now - 1.hour, questionnaire_id: 98, parent_id: 12112, type: 'CourseSurveyDeployment')
       expect(sd).not_to be_valid
       expect(sd.errors[:base]).to include('The End Date should be in the future.')
     end
 
     it 'is valid with correct start_date and end_date' do
-      sd = SurveyDeployment.new(start_date: Time.now, end_date: Time.now + 1.day)
+      sd = SurveyDeployment.new(start_date: Time.now, end_date: Time.now + 1.day, questionnaire_id: 98, parent_id: 12112, type: 'CourseSurveyDeployment')
       expect(sd).to be_valid
     end
   end
 
   describe 'abstract methods' do
-    let(:sd) { SurveyDeployment.new(start_date: Time.now, end_date: Time.now + 1.day) }
+    let(:survey_deployment) { SurveyDeployment.new(start_date: Time.now, end_date: Time.now + 1.day) }
 
     it 'responds to parent_name' do
-      expect { sd.parent_name }.not_to raise_error
+      expect { survey_deployment.parent_name }.not_to raise_error
     end
 
     it 'responds to response_maps' do
-      expect { sd.response_maps }.not_to raise_error
+      expect { survey_deployment.response_maps }.not_to raise_error
+    end
+
+    it 'should return the associated response map' do
+      survey_deployment = AssignmentSurveyDeployment.new(questionnaire_id: 986, start_date: previous_day, end_date: nil, parent_id: '12345678', type: 'AssignmentSurveyDeployment', id: 1)
+      allow(AssignmentSurveyResponseMap).to receive(:where).with(reviewee_id: 1).and_return([response_map])
+      expect(survey_deployment.response_maps).to eq([response_map])
     end
   end
 end
