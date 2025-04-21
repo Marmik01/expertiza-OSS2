9d7297fba09286a15b1016abc11783631385a8b8
Marmik01 <marmikpatel01@gmail.com>
2025-04-21 12:35:32 -0400

    Added some new tests

--- spec/helpers/survey_deployment_helper_spec.rb
+++ spec/helpers/survey_deployment_helper_spec.rb
@@ -48,32 +48,40 @@ RSpec.describe SurveyDeploymentHelper, type: :helper do
       expect(result[3]).to eq(2)
     end
 
-    # new: handles invalid survey_deployment id
+    # handles invalid survey_deployment id
     it 'returns all zeros if survey_deployment id is invalid' do
       result = helper.get_responses_for_question_in_a_survey_deployment(question.id, -1)
       expect(result).to eq([0, 0, 0, 0, 0, 0])
     end
 
-    # new: handles invalid question id
+    # handles invalid question id
     it 'raises error when invalid question id is used' do
       expect {
         helper.get_responses_for_question_in_a_survey_deployment(-123, survey_deployment.id)
       }.to raise_error(ActiveRecord::RecordNotFound)
     end
 
-    # new: handles nil @range_of_scores
+    # handles nil @range_of_scores
     it 'returns empty array when @range_of_scores is nil' do
       helper.instance_variable_set(:@range_of_scores, nil)
       result = helper.get_responses_for_question_in_a_survey_deployment(question.id, survey_deployment.id)
       expect(result).to eq([])
     end
 
-    # new: handles custom score range
+    # handles custom score range
     it 'works correctly with custom @range_of_scores (1 to 3)' do
       helper.instance_variable_set(:@range_of_scores, (1..3).to_a)
       create(:answer, question_id: question.id, response_id: response.id, answer: 2)
       result = helper.get_responses_for_question_in_a_survey_deployment(question.id, survey_deployment.id)
       expect(result).to eq([0, 1, 0])
+    end
+
+    # new
+    it 'ignores answers with scores outside the @range_of_scores' do
+      create(:answer, question_id: question.id, response_id: response.id, answer: 10)
+      create(:answer, question_id: question.id, response_id: response.id, answer: 2)
+      result = helper.get_responses_for_question_in_a_survey_deployment(question.id, survey_deployment.id)
+      expect(result).to eq([0, 0, 1, 0, 0, 0])
     end
   end
 
@@ -103,10 +111,16 @@ RSpec.describe SurveyDeploymentHelper, type: :helper do
       expect(helper.allowed_question_type?(question)).to be false
     end
 
-    # new: verifies a type that is unknown/unsupported
+    # verifies a type that is unknown/unsupported
     it 'returns false for an unknown type' do
       question = double('Question', type: 'Dropdown')
       expect(helper.allowed_question_type?(question)).to be false
+    end
+
+    # new
+    it 'works with a real Criterion question model' do
+      real_question = create(:question, type: 'Criterion')
+      expect(helper.allowed_question_type?(real_question)).to be true
     end
   end
 end
